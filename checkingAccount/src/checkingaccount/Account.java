618ac9d6fca1c60fff45478c58a6378cdc3b225e
Kevin Bell <bellKevin@pm.me>
2022-06-02 02:55:51 +0000

    Add files via upload

---
+++ checkingAccount/src/checkingaccount/Account.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package checkingaccount;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author 4800590195
+ */
+class Account {
+
+    public double balance, fee, amount;
+
+    public void setBalance() {
+
+    }
+
+    public void setFee() {
+
+    }
+
+    public void getBalance() {
+
+    }
+
+    public void withdrawl() {
+
+    }
+
+    public void deposit() {
+
+    }
+}
