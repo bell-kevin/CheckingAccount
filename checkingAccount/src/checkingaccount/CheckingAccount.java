cdd060a175f7157dba4450f3604904761732f401
Kevin Bell <bellKevin@pm.me>
2022-06-02 14:37:46 +0000

    Update CheckingAccount.java

--- checkingAccount/src/checkingaccount/CheckingAccount.java
+++ checkingAccount/src/checkingaccount/CheckingAccount.java
@@ -1,30 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package checkingaccount;
 
 import java.util.Scanner;
 
-/**
- *
- * @author 4800590195
- */
 public class CheckingAccount {
 
     private double balance, fee;
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
         Scanner computerKeyboardInput = new Scanner(System.in);
         System.out.println("Checking Account by Kevin Bell\n");
         Account checking;
         checking = new Account();
         checking.
     }
-
 }
