5b56c1bc2dc88d064402543cc0fef87c5da87479
Kevin Bell <bellKevin@pm.me>
2022-06-02 16:52:57 +0000

    Update CheckingAccount.java

--- checkingAccount/src/checkingaccount/CheckingAccount.java
+++ checkingAccount/src/checkingaccount/CheckingAccount.java
@@ -4,13 +4,40 @@ import java.util.Scanner;
 
 public class CheckingAccount {
 
-    private double balance, fee;
+    private double balance, fee, amount;
 
     public static void main(String[] args) {
+        double balance, fee;
+        char d, w, q;
+        String response;
         Scanner computerKeyboardInput = new Scanner(System.in);
         System.out.println("Checking Account by Kevin Bell\n");
         Account checking;
         checking = new Account();
-        checking.
+        System.out.print("New Checking Account! What is the starting balance? ");
+        balance = computerKeyboardInput.nextDouble();
+        checking.setBalance();
+        System.out.print("What is the fee for this checking account? ");
+        fee = computerKeyboardInput.nextDouble();
+        checking.setFee();
+        System.out.println("");
+        System.out.print("Please select deposit (d), withdrawal (w), or quit (q): ");
+        response = computerKeyboardInput.next();
+        while (response == 'w' || response == 'd' || response == 'q') {
+            System.out.println("Please select deposit (d), withdrawal (w), or quit (q): ");
+            response = computerKeyboardInput.nextLine().charAt(0);
+            switch (response.charAt(0)) {
+                case 'd':
+                    checking.deposit();
+                    break;
+                case 'w':
+                    checking.withdrawl();
+                    break;
+                case 'q':
+                    break;
+                default:
+                     System.out.println("Please select deposit (d), withdrawal (w), or quit (q): ");
+            }
+        }
     }
 }
