ace03aba55170c2767ee956d7913e5a78e134cac
Kevin Bell <bellKevin@pm.me>
2022-06-02 17:13:42 +0000

    Update Account.java

--- checkingAccount/src/checkingaccount/Account.java
+++ checkingAccount/src/checkingaccount/Account.java
@@ -12,15 +12,16 @@ class Account {
         this.fee = fee;
     }
 
-    public double getBalance() {
-        return this.balance;
-    }
-
     public void withdrawl() {
         amount = (this.balance - this.amount) - fee;
     }
 
     public void deposit() {
-        amount = (this.balance + this.amount) - fee;
+        balance = (this.balance + this.amount) - fee;
+    }
+
+    public double getBalance(double balance) {
+        System.out.println("New balance: $" + balance);
+        return this.balance;
     }
 }
