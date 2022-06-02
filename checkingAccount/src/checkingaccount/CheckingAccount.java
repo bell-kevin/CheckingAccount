11272122a32e353a1adbae5dcdebe698d761b62a
Kevin Bell <bellKevin@pm.me>
2022-06-02 19:00:00 +0000

    Update CheckingAccount.java

--- checkingAccount/src/checkingaccount/CheckingAccount.java
+++ checkingAccount/src/checkingaccount/CheckingAccount.java
@@ -4,11 +4,8 @@ import java.util.Scanner;
 
 public class CheckingAccount {
 
-    private double balance, fee, amount; //needs to be private. how?
-
     public static void main(String[] args) {
         double balance, fee, amount;
-        char d, w, q;
         String response;
         Scanner computerKeyboardInput = new Scanner(System.in);
         System.out.println("Checking Account by Kevin Bell\n");
@@ -16,33 +13,29 @@ public class CheckingAccount {
         checking = new Account();
         System.out.print("New Checking Account! What is the starting balance? $");
         balance = computerKeyboardInput.nextDouble();
-        checking.setBalance();
+        checking.setBalance(balance);
         System.out.print("What is the fee for this checking account? $");
         fee = computerKeyboardInput.nextDouble();
-        checking.setFee();
+        checking.setFee(fee);
         System.out.println("");
-        System.out.print("Please select deposit (d), withdrawal (w), or quit (q): ");
-        response = computerKeyboardInput.next();
-        System.out.print("How much will you deposit? $");
-        amount = computerKeyboardInput.nextDouble();
-        balance = balance - fee;
-        balance = balance + amount;
-        checking.getBalance(balance);
-        while (response == "d") {
-            System.out.println("Please select deposit (d), withdrawal (w), or quit (q): ");
-            System.out.println("How much will you deposit? ");
-            response = computerKeyboardInput.nextLine().charAt(0);
+        for (int i = 0; i < 3; i++) {
+            System.out.print("Please select deposit (d), withdrawal (w), or quit (q): ");
+            response = computerKeyboardInput.next();
             switch (response.charAt(0)) {
                 case 'd':
-                    checking.deposit();
+                    System.out.print("How much will you deposit? $");
+                    amount = computerKeyboardInput.nextDouble();
+                    checking.deposit(amount);
+                    checking.getBalance();
                     break;
                 case 'w':
-                    checking.withdrawl();
+                    System.out.print("How much will you withdraw? $");
+                    amount = computerKeyboardInput.nextDouble();
+                    checking.withdrawl(amount);
+                    checking.getBalance();
                     break;
                 case 'q':
                     break;
-                default:
-                    System.out.println("Please select deposit (d), withdrawal (w), or quit (q): ");
             }
         }
     }
