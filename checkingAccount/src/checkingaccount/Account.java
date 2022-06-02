42eeb23407372d85b91d516484aefcc2421d4b7f
Kevin Bell <bellKevin@pm.me>
2022-06-02 14:39:01 +0000

    Update Account.java

--- checkingAccount/src/checkingaccount/Account.java
+++ checkingAccount/src/checkingaccount/Account.java
@@ -1,16 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package checkingaccount;
 
 import java.util.Scanner;
 
-/**
- *
- * @author 4800590195
- */
 class Account {
 
     public double balance, fee, amount;
